47467b0e4a51750245165d9a5be381647a9fb412
Lini Rose <75529247+LiniRose@users.noreply.github.com>
2026-09-21 18:59:14 +0200

    program now uses the filerepositories...

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -1,11 +1,11 @@
 ﻿using CLI.UI;
-using InMemoryRepositories;
+using FileRepositories;
 using RepositoryContracts;
 
 Console.WriteLine("Starting CLI app...");
-IUserRepository userRepository = new UserInMemoryRepository();
-ICommentRepository commentRepository = new CommentInMemoryRepository();
-IPostRepository postRepository = new PostInMemoryRepository();
+IUserRepository userRepository = new UserFileRepository();
+ICommentRepository commentRepository = new CommentFileRepository();
+IPostRepository postRepository = new PostFileRepository();
 
 CliApp app = new CliApp(userRepository, commentRepository, postRepository);
 await app.StartAsync();
